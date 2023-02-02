a243f50a2c8b48b1a31e4f0ab50c26cd444b80f9
Tejas1409 <tejmene123@gmail.com>
2023-02-02 12:16:18 +0530

    encap

--- src/main/java/com/driver/RWOnly.java
+++ src/main/java/com/driver/RWOnly.java
@@ -1,5 +1,13 @@
 package com.driver;
 
 public class RWOnly {
-    
+    private String a;
+
+    public String getA() {
+        return a;
+    }
+
+    public void setA(String a) {
+        this.a = a;
+    }
 }
